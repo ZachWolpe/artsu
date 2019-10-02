ed14d2b0908424e1f67d9758314cc09491186b49
ZachWolpe <zachcolinwolpe@gmail.com>
2019-10-02 14:12:11 +0200

    first

--- app.py
+++ app.py
@@ -1,1 +1,11 @@
-from flask import Flask
+from flask import flask
+
+app = flask(__name__)
+
+@app.route('/')
+def index():
+    return "<h1>hello Flask</h1>"
+
+
+if __name__ == "__main__":
+    app.run()
